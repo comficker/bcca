34fead21c71f28ff933ef4fce641451c079d5aab
comficker <comficker@gmail.com>
2023-12-02 08:55:38 +0700

    init

--- pages/news.vue
+++ pages/news.vue
@@ -46,56 +46,24 @@ const clubs = [
 ]
 
 
-const news = [
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#323A57"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#EBB29A"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#9FD0CC"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#9FD0CC"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#9FD0CC"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#9FD0CC"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#9FD0CC"
-  },
-  {
-    image: "https://supapjpiqdfzuaordcdx.supabase.co/storage/v1/render/image/public/images/thelever.jpeg?width=600&height=426&resize=cover",
-    title: "BLAG (Better Letters Magazine)",
-    desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
-    c: "#9FD0CC"
-  }
+const safes = [
+  "thailand-motorcycle",
+  "asia",
+  "combodia-motorcycle",
+  "indonesia-car",
+  "iran",
+  "saferiders",
+  "singapore-motocycle"
 ]
+
+const news = computed(() => {
+  return safes.map(x => ({
+      image: `/post/${x}.JPG`,
+      title: "BLAG (Better Letters Magazine)",
+      desc: "Award-winning journalist and Oscar-nominated writer David Sirota conducts investigative journalism about holding people and corporations manipulating the levers of power accountable.",
+      c: "#9FD0CC"
+    }))
+})
 </script>
 
 <template>
