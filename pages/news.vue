8478fff04a3802dbdb43af965c206d3730bd737f
comficker <comficker@gmail.com>
2023-12-02 12:36:38 +0700

    init

--- pages/news.vue
+++ pages/news.vue
@@ -72,7 +72,7 @@ const news = computed(() => {
       <div class="relative bg-gray-800 px-6 py-32 sm:px-12 sm:py-40 lg:px-16 rounded-lg overflow-hidden">
         <div class="absolute inset-0 overflow-hidden">
           <img
-            src="https://tailwindui.com/img/ecommerce-images/home-page-03-feature-section-full-width.jpg" alt=""
+            src="/club/Car-Club-Singapore.jpg" alt=""
             class="h-full w-full object-cover object-center"/>
         </div>
         <div aria-hidden="true" class="absolute inset-0 bg-gray-900 bg-opacity-50"/>
